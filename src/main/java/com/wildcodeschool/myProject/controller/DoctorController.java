40a692dd3dac714cd6e305cde9eaf3f3102e952f
Samuel-Brr <samuel.barre972@gmail.com>
2022-07-24 18:46:52 +0200

    ok

---
+++ src/main/java/com/wildcodeschool/myProject/controller/DoctorController.java
@@ -0,0 +1,26 @@
+package com.wildcodeschool.myProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DoctorController {
+    @GetMapping("/doctor/1")
+    @ResponseBody
+    public String index() {
+        return "William Hartnell";
+    }
+
+    @GetMapping("/doctor/10")
+    @ResponseBody
+    public String doc10() {
+        return "David Tennant";
+    }
+
+    @GetMapping("/doctor/13")
+    @ResponseBody
+    public String doc13() {
+        return "Jodie Whittaker";
+    }
+}
